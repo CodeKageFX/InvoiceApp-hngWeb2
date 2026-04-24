839f812e1a7438027dbea1bef863cdfaabd76ca0
CodeKageFX <luqmanola60@gmail.com>
2026-04-24 08:36:57 +0100

    fix: responsiveness 2.0

--- src/app/invoices/[id]/page.tsx
+++ src/app/invoices/[id]/page.tsx
@@ -3,7 +3,7 @@ import InvoiceDetails from "@/components/InvoiceDetails";
 export default async function InvoicePage({params}: {params: Promise<{id: string}>}) {
     const { id } = await params
     return (
-        <section className="text-4xl w-[730px] max-[859px]:w-[80%] mx-auto mt-10">
+        <section className="text-4xl w-[730px] max-[859px]:w-[80%] max-[577px]:w-[95%] mx-auto mt-10">
             <InvoiceDetails id={id} />
         </section>
     )
